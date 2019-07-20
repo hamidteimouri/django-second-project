bceecbd61515d5a0eea6135bee0d7cd07aeb49da
hamidteimouri <hamidteimouri@outlook.com>
2019-07-20 17:32:37 +0430

    template directory

--- first_project/settings.py
+++ first_project/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Added by developer
+TEMPLATE_DIR = os.path.join(BASE_DIR + "templates")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],                                       # edited by developer
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
